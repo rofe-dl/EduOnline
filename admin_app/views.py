d30bc640c0a03b48395756fccb8d8591378df080
rofe-dl <rafidul258@gmail.com>
2021-04-21 19:51:52 +0600

    finished edit question details only

--- admin_app/views.py
+++ admin_app/views.py
@@ -18,10 +18,13 @@ create_exam_details_url = 'admin_app/create_exam_details.html'
 create_exam_questions_url = 'admin_app/create_exam_questions.html'
 edit_exam_details_url = 'admin_app/edit_exam_details.html'
 
-#TODO Prevent users from accessing administrator panel
+#TODO Prevent users from accessing administrator panel, vice versa
 #TODO Grey out submit button for question if no changes made
 #TODO Update total marks of exam upon addition and deletion of question
 #TODO Download jquery
+#TODO Save and Edit paper buttons different
+#TODO prevent default when question added to paper
+#TODO trim input when taking making question
 
 @login_required(login_url=login_url)
 def index(request):
@@ -85,7 +88,8 @@ def create_exam_details_view(request):
 @login_required(login_url=login_url)
 def create_exam_questions_view(request, exam_id):
     if request.method == "POST":
-
+        
+        # creates question object with solution_id not set because it's not known
         question_id = "q-" + str(uuid.uuid4())
         question = Question(
             question_id=question_id,
@@ -95,6 +99,8 @@ def create_exam_questions_view(request, exam_id):
         )
         question.save()
         
+
+        # creates choice objects and set the solution id
         solution = request.POST["solution"]
         choices = request.POST.getlist('choice')
 
@@ -111,7 +117,7 @@ def create_exam_questions_view(request, exam_id):
             if(choice == solution):
                 solution_id = choice_id
 
-
+        # adds the solution to the previous question object
         setattr(question, "solution_id", solution_id)
         question.save()
 
@@ -129,7 +135,24 @@ def delete_exam_view(request, exam_id):
 @login_required(login_url=login_url)
 def edit_exam_details_view(request, exam_id):
     if request.method == "POST":
-        pass
+
+        # if just the details are edited
+        exam = Exam.objects.get(exam_id=exam_id)
+
+        exam.exam_name = request.POST["exam_name"]
+        exam.duration = request.POST["duration"]
+        exam.standard = request.POST["standard"]
+        exam.subject = Subject.objects.get(subject_name=request.POST["subject_name"])
+
+        exam.save()
+
+        if "save_details" in request.POST:
+            return HttpResponseRedirect(reverse("admin_app:exams"))
+        
+        elif "edit_questions" in request.POST:
+            pass
+
+
 
     exam = Exam.objects.get(exam_id=exam_id)
     return render(request, edit_exam_details_url, {
