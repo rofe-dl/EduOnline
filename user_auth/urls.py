05de6b8ec14f0c66da55b29745a528dc9bba906f
rofe-dl <rafidul258@gmail.com>
2021-04-24 20:59:40 +0600

    finished user registration

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -6,5 +6,6 @@ app_name = 'user_auth'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout')
 ]
--- user_auth/views.py
+++ user_auth/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+from .models import *
+
 index_html_dir = "user_auth/index.html"
 
 def index(request):
@@ -21,18 +23,51 @@ def index(request):
 
             if(user.profile.is_admin):
                 return HttpResponseRedirect(reverse('admin_app:index'))
-            # else:
-            #     return HttpResponseRedirect(reverse('student_app:index'))
+            else:
+                return HttpResponseRedirect(reverse('student_app:index'))
 
         else:
             return render(request, index_html_dir, {
-                "message" : "Username or Password is incorrect",
-                "username" : username
+                "login_message" : "Username or Password is incorrect",
+                "login_username" : username
             })
     
     return render(request, index_html_dir)
 
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        confirm_password = request.POST["confirm_password"]
+
+        if not password == confirm_password:
+            return render(request, index_html_dir, {
+                "register_username" : username,
+                "register_email" : email,
+                "register_message" : "Passwords do not match"
+            })
+
+        elif User.objects.filter(username=username).exists():
+            return render(request, index_html_dir, {
+                "register_username" : username,
+                "register_email" : email,
+                "register_message" : "Username already exists"
+            })
+
+        user = User(username=username, password=password)
+        user.save()
+
+        profile = Profile(is_admin=False, user=user)
+        profile.save()
+        
+        login(request, user)
+
+        return HttpResponseRedirect(reverse('admin_app:index'))
+
+
 @login_required(login_url='/')
 def logout_view(request):
+
     logout(request)
     return HttpResponseRedirect(reverse('user_auth:index'))
